5307a1bedd927857e21d4faf92cd34940646125b
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-04 15:02:26 -0700

    Added basic content to the BaseContent component and added it to the Home view

--- src/components/content/BaseContent.vue
+++ src/components/content/BaseContent.vue
@@ -1,15 +1,41 @@
 <template>
-  <div>
-    
+  <div class="base-content">
+    <p>BaseContent</p>
+    <h1>{{ head }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "BaseContent"
+  name: "BaseContent",
+  props: {
+    head: String
+  }
 }
 </script>
 
 <style lang="sass" scoped>
+.base-content
+  border: 2px solid black;
 
+h3
+  margin: 40px 0 0;
+
+ul
+  list-style-type: none;
+  padding: 0;
+
+li
+  display: inline-block;
+  margin: 0 10px;
+  
+a
+  color: #42b983;
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,22 +2,22 @@
   <div class="home">
     <Header />
     <Action />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <BaseContent head="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import Action from '../components/layout/Action.vue';
 import Header from '../components/layout/Header.vue';
+import BaseContent from '../components/content/BaseContent.vue';
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
     Action,
-    Header
+    Header,
+    BaseContent
   }
 };
 </script>
