a02eadf6e341f27a3fe8611610f59532768358db
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-04 14:30:06 -0700

    created the Action component for driving conversions; added identifier styling

---
+++ src/components/Action.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="action">
+    <p>Action</p>
+    <button>I Need A Website</button>
+    &nbsp;
+    <button>I Need A Developer</button>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="sass" scoped>
+.action
+  border: 2px solid black;
+  padding: 1em;
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <Action />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -8,11 +9,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Action from '../components/Action.vue';
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Action
   }
 };
 </script>
