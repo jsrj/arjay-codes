5ce7372c95d1f4cc287c2def4548713f8235acc4
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-04 14:26:23 -0700

    added Navbar to App component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="navbar">
-    <a>__LOGO__</a>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
 </template>
 
@@ -11,6 +12,12 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-
+.navbar
+  padding: 30px;
+  a
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active
+      color: #42b983;
 </style>
 
