8aea69740bbf16406daa90b85f234b1a2fc186ca
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-05 02:25:00 -0700

    stylized the 404 page

--- src/views/server/NotFound.vue
+++ src/views/server/NotFound.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <br />
-    <img src="https://media.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif">
+    <div id="image-frame">
+      <img src="https://media.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif">
+    </div>
     <h1>Page Not Found</h1>
   </div>
 </template>
@@ -13,6 +15,17 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+#image-frame
+  border: 12px solid black;
+  border-radius: 100%;
+  display: flex;
+  margin: 0 auto;
+  width: min-content;
 
+  img
+    margin: 0;
+    border: 6px dashed black;
+    border-radius: 100%;
+    box-shadow: 0px 0px 0px 6px #87ceeb;
 </style>
 
