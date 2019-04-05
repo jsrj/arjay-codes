ea1e466ea55adb482140c0d2d41a5b503d5c8384
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-05 00:20:49 -0700

    added an identifier tag and border to footer for layout planning

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,15 +1,24 @@
 <template>
-  
+  <div id="footer">
+    <DevIdentifierTag tagName="footer" />
+  </div>
 </template>
 
 <script>
+import DevIdentifierTag from '@/components/helpers/DevIdentifierTag.vue';
+
 export default {
-  
+  name: 'Footer',
+  components: {
+    DevIdentifierTag
+  },
+  props: {}
 }
 </script>
 
 <style lang="sass" scoped>
-
+  #footer
+    border: 2px solid black;
 </style>
 
 
