caa12656ec63f15a542aa7c4573c049cd15014ce
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-05 02:06:05 -0700

    added views subdirectory for error pages; added a 404 page

---
+++ src/views/server/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <br />
+    <img src="https://media.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif">
+    <h1>Page Not Found</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
+
