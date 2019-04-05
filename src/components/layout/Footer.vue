44d2a120d4feea7b6b1e980d355b9b972e0bdd59
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-05 00:37:56 -0700

    added copyright info with layout development guide border

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="footer">
     <DevIdentifierTag tagName="footer" />
+    <div id="footer-content">
+      <small>&copy; {{currentYear}} Arjay Codes, all rights reserved. Powered by〘❤️ && ☕️〙.</small>
+    </div>
   </div>
 </template>
 
@@ -12,13 +15,26 @@ export default {
   components: {
     DevIdentifierTag
   },
-  props: {}
+  props: {},
+  data() {
+    return {
+      currentYear: new Date().getFullYear()
+    }
+  }
 }
 </script>
 
 <style lang="sass" scoped>
   #footer
     border: 2px solid black;
+
+  #footer-content
+    display: flex;
+    flex-direction: row;
+    margin-left: 0.35rem;
+
+    small
+      border: 2px solid red;
 </style>
 
 
