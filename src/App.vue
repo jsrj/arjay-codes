0bee7157ac19d77c18fbc876c1c4ea5b64f5d9ef
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-06 02:20:22 -0700

    added the created() method to App for auth token renew

--- src/App.vue
+++ src/App.vue
@@ -40,6 +40,14 @@ export default {
         }
       ]
     }
+  },
+  async created() {
+    try {
+      await this.$auth.renewTokens();
+    } catch {
+      // Supress the 'not logged in' error as we can illegitimately get that
+      // when processing the callback url
+    }
   }
 }
 </script>
