d418e3308c908e57d0e2867c443ea9909cb74bf1
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-06 23:18:43 -0700

    re-enabled Meta component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="home">
-    <!-- <Meta 
+    <Meta 
       :title="title" 
       :subTitle="subTitle" 
       :description="description" 
-    /> -->
+    />
     <Header />
     <Action />
     <BaseContent 
