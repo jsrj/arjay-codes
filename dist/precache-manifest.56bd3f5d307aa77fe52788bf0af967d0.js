85d7dde5d59ee08da68922526e116f5be16f15e0
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-07 23:11:47 -0700

    adding dist bild directory for netlify deployment (preserving docker config for eventual EBS scalable deployments as well)

---
+++ dist/precache-manifest.56bd3f5d307aa77fe52788bf0af967d0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "48a12ad88261d868549b41ad44339d96",
+    "url": "/plugins/reset.css"
+  },
+  {
+    "revision": "112272e51c80ffe5bd01becd2ce7d656",
+    "url": "/plugins/normalize.css"
+  },
+  {
+    "revision": "0ea1029999cb47098610",
+    "url": "/js/chunk-vendors.e47af9f9.js"
+  },
+  {
+    "revision": "5427fc89940060899408",
+    "url": "/js/app.3894e3b0.js"
+  },
+  {
+    "revision": "6ce34246538c6aa5a84a488ed66a9737",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/img/loading.c5590569.svg"
+  },
+  {
+    "revision": "5427fc89940060899408",
+    "url": "/css/app.6a94cf86.css"
+  }
+];
