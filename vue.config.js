4e4386bda2efdaa097e16b314cf885257c1bf865
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-08 00:13:56 -0700

    removed jquery reference from vue config

--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,5 @@ const webpack = require("webpack");
 module.exports = {
   devServer: {
     port: 8080
-  },
-  configureWebpack: {
-    plugins: [
-      new webpack.ProvidePlugin({
-        $: "jquery",
-        jquery: "jquery",
-        "window.jQuery": "jquery",
-        jQuery: "jquery"
-      })
-    ]
   }
 };
