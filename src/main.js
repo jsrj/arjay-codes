94430d5d8776fc845064ecbdac7e28a34a49f994
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-08 00:01:14 -0700

    cleaned up main.js and added vuetify

--- src/main.js
+++ src/main.js
@@ -2,16 +2,22 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Vuetify from "vuetify";
 import vueHeadful from "vue-headful";
 import AuthPlugin from "./plugins/auth";
 import HighlightJs from "./directives/highlight";
 import "./registerServiceWorker";
 
-Vue.use(AuthPlugin);
-Vue.directive("highlightjs", HighlightJs);
-
 Vue.config.productionTip = true;
 
+// Vue Use declarations
+Vue.use(Vuetify);
+Vue.use(AuthPlugin);
+
+// Vue Directive declarations
+Vue.directive("highlightjs", HighlightJs);
+
+// Vue Global Component declarations
 Vue.component("vue-headful", vueHeadful);
 
 new Vue({
