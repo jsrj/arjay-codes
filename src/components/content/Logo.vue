da96620f72bad774ed34dacfd70f3f6226d42132
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-08 22:56:47 -0700

    created a styled text logo component

---
+++ src/components/content/Logo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="outer-border">
+    <div class="logo-content">
+      A|C
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logo'
+}
+</script>
+
+<style lang="sass" scoped>
+.outer-border
+  border: 3px solid black;
+  border-radius: 100%;
+  box-shadow: 0px 0px 0px 3px orange;
+  margin: 1rem;
+
+  .logo-content
+    align-items: center;
+    background-color: white;
+    border-radius: 100%;
+    color: black;
+    display: flex;
+    height: 2em;
+    justify-content: center;
+    padding: 1rem;
+    width: 2em;
+</style>
+
