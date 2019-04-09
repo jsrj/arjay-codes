9c4b52d8d8c42e3fe7c4a6f2521fd7e3193de6cb
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-09 15:57:15 -0700

    stubbed out Component for housing navigation links

---
+++ src/components/layout/navigation/NaviLinks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NaviLinks',
+  components: {},
+  props: {},
+  data() {},
+  mounted() {},
+  methods() {}
+}
+</script>
+
+
