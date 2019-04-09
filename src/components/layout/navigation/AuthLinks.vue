20c681cc44b601da1720c41d1c7594db0dbfdecd
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-09 16:15:52 -0700

    added authentication methods

--- src/components/layout/navigation/AuthLinks.vue
+++ src/components/layout/navigation/AuthLinks.vue
@@ -1,4 +1,9 @@
-<template>  
+<template>
+<div>
+  <!-- IF user is not logged in, THEN display login - register button -->
+  
+  <!-- IF user is logged in, THEN do not display login - register button -->
+</div>
 </template>
 
 <script>
@@ -6,9 +11,27 @@ export default {
   name: 'AuthLinks',
   components: {},
   props: {},
-  data() {},
+  data() {
+    return {
+      isAuthenticated: false,
+      profile: {},
+      drawer: null,
+    }
+  },
   mounted() {},
-  methods() {}
+  methods: {
+    login() {
+      this.$auth.login();
+    },
+    logout() {
+      this.$auth.logOut();
+      this.$router.push({ path: "/" });
+    },
+    handleLoginEvent(data) {
+      this.isAuthenticated = data.loggedIn;
+      this.profile = data.profile;
+    },
+  }
 }
 </script>
 
