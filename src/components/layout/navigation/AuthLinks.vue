15c1251f62c2e93f22e007bf15656db46d140439
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-09 13:18:12 -0700

    created component stub for managing the reactive link for managing user authentication and authenticated user profile views

---
+++ src/components/layout/navigation/AuthLinks.vue
@@ -0,0 +1,17 @@
+<template>  
+</template>
+
+<script>
+export default {
+  name: 'AuthLinks',
+  components: {},
+  props: {},
+  data() {},
+  mounted() {},
+  methods() {}
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
+
