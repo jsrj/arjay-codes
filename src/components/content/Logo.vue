4346c52082eba69cc97ecd3d5fd7f4f4d2f41843
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-09 01:11:59 -0700

    leveraged vuetify bottom property to calculate the positioning of the Logo instead of manually writing the same code for said calculation; removed un-needed stylings

--- src/components/content/Logo.vue
+++ src/components/content/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-btn fab large :loading="contentLoading" class="outer-border">
+  <v-btn fab large absolute bottom :loading="contentLoading" class="outer-border">
     <div id="inner-wrapper">
       <div id="hemi-top">
         <span>Arjay</span>
@@ -27,25 +27,25 @@ export default {
 .outer-border
   border: 6px solid orange;
   box-shadow: 0px 0px 2px 2px black;
-  margin-top: 12.4vh;
   overflow: hidden;
   padding: 0px;
 
   #inner-wrapper
-    margin: 0px;
-    padding: 0px;
+    height: 100%;
 
   #hemi-top
     background-color: white;
-    border-radius: 100em 100em 0 0;
     color: black;
-    margin: 0px;
     width: 100%;
     height: 50%;
+    span
+      margin-top: 50%;
 
   #hemi-bot
     background-color: black important;
     color: white;
     width: 100%;
     height: 50%;
+    span
+      margin: 20px;
 </style>
