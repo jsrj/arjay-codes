e6b14905603d066a4038a5796239326df27c50ad
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-08 22:57:19 -0700

    added Logo component to navbar title

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,45 +1,20 @@
 <template>
   <v-layout v-resize="onResize" wrap>
-    <!-- 
-      TODO: Configure so that nav drawer is shown on desktop, but v-toolbar is shown on mobile devices. 
-      DO NOT display both since the page quickly becomes way too crowded. 
-    -->
     <!-- Nav Drawer -->
         <!-- Main Toolbar -->
     <v-toolbar app dark>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Arjay | Codes</v-toolbar-title>
+      <!-- <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon> -->
+      <v-toolbar-title>
+        <Logo />
+      </v-toolbar-title>
+      
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <router-link active tag="v-btn" flat to="/">Home</router-link>
+        <router-link tag="v-btn" flat to="/">Home</router-link>
         <router-link tag="v-btn" flat to="/about">About</router-link>
         <v-btn  color="orange" @click.prevent="login"> Login - Register</v-btn>
       </v-toolbar-items>
     </v-toolbar>
-
-    <v-navigation-drawer app dark :width="(this.windowSize.x < 800) ? this.windowSize.x : 125" v-model="drawer">
-      <v-list class="pa-1" centered>
-        <v-list-tile avatar centered>
-          <v-list-tile-avatar centered>
-            <img src="https://randomuser.me/api/portraits/men/85.jpg" centered>
-          </v-list-tile-avatar>
-        </v-list-tile>
-      </v-list>
-
-      <v-list class="pt-0" dense>
-        <v-divider></v-divider>
-
-        <router-link tag="v-list-tile" v-for="item in items" :key="item.title" :to="item.target">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </router-link>
-      </v-list>
-    </v-navigation-drawer>
   </v-layout>
   <!-- <v-navigation-drawer permanent app :v-model="null" :width="width" height="100vh">
 
@@ -62,9 +37,13 @@
 </template>
 
 <script>
+import Logo from '../content/Logo.vue';
+
 export default {
   name: "Navbar",
-  components: {},
+  components: {
+    Logo
+  },
   props: {
     width: String
   },
@@ -117,4 +96,3 @@ export default {
 
 <style lang="sass" scoped>
 </style>
-
