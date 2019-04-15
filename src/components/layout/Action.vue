32a939c86655b67cf3c28750f6e2c949462bc46e
Arjay Jones <robert.jones@arjaycodes.com>
2019-04-14 19:15:44 -0700

    changed action heading

--- src/components/layout/Action.vue
+++ src/components/layout/Action.vue
@@ -4,7 +4,7 @@
 
       {{ /* TOP SECTION - START */  }}
       <v-layout column wrap>
-        <v-flex text-lg-left class="display-1 font-weight-bold">I Know What I Want</v-flex>
+        <v-flex text-lg-left class="display-1 font-weight-bold">Know What You Need?</v-flex>
         <v-flex text-md-left class="blockquote">
           Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live theblind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large languageocean. A small river named Duden flows by
           their place and supplies it with the necessary regelialia.
@@ -15,21 +15,23 @@
       <v-divider></v-divider>
       
       {{ /* BOTTOM SECTION - START */  }}
-      <v-layout row wrap ma-1 pa-1>
-        <v-flex>
+      <v-layout row wrap ma-1 pa-2>
+        <v-flex ma-2>
           <v-img alt="image" src="http://lorempixel.com/g/200/200/business/" />
         </v-flex>
 
         <v-flex>
-          <h2><strong>Sub-Title</strong></h2>
-          <p class="lead">Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.</p>
+          <v-layout column pa-2>
+            <v-flex text-lg-left>Sub-Title</v-flex>
+            <v-flex text-md-left>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.</v-flex>
+          </v-layout>
         </v-flex>
 
         <v-flex>
           <v-list>
             <v-list-tile><v-btn append to="#">I Need A Website</v-btn></v-list-tile>
             <v-list-tile><v-btn append to="#">I Need A Developer</v-btn></v-list-tile>
-            <v-list-tile><em>Some foo text</em></v-list-tile>
+            <v-list-tile><em>Or read on...</em></v-list-tile>
           </v-list>
         </v-flex>
       </v-layout>
