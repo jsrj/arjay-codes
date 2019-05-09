fa04ca04624bb03651c602a41a9224ada8d51c3b
Arjay Jones <robert.jones@arjaycodes.com>
2019-05-09 15:44:48 -0700

    created a Social Links component stub

---
+++ src/components/layout/navigation/SocialLinks.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="social-links">
+    <!-- ... -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SocialLinks",
+  components: {},
+  props: {},
+  methods: {},
+  mounted() {},
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  /* Page-global Variables  */
+  /* Sub-component Variables*/
+  /*------------------------*/
+  /* Styles by Element */
+  /* Styles by Class   */
+  /* Styles by ID      */
+</style>
+
+
