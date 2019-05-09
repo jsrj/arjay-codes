4b31cc566e42c160fc32ae01efb5f98679c93062
Arjay Jones <robert.jones@arjaycodes.com>
2019-05-09 16:42:57 -0700

    wrapped icons in anchor tags in SocialLinks

--- src/components/layout/navigation/SocialLinks.vue
+++ src/components/layout/navigation/SocialLinks.vue
@@ -1,10 +1,20 @@
 <template>
   <div id="social-links">
-    <v-icon class="brand" id="facebook"  name="brands/facebook"       />
-    <v-icon class="brand" id="twitter"   name="brands/twitter-square" />
-    <v-icon class="brand" id="instagram" name="brands/instagram"      />
-    <v-icon class="brand" id="github"    name="brands/github-square"  />
-    <v-icon class="brand" id="linkedin"  name="brands/linkedin"       />
+    <a href="#" target="_blank">
+      <v-icon class="brand" id="facebook"  name="brands/facebook"       />
+    </a>
+    <a href="#" target="_blank">
+      <v-icon class="brand" id="twitter"   name="brands/twitter-square" />
+    </a>
+    <a href="#" target="_blank">
+      <v-icon class="brand" id="instagram" name="brands/instagram"      />
+    </a>
+    <a href="#" target="_blank">
+      <v-icon class="brand" id="github"    name="brands/github-square"  />
+    </a>
+    <a href="#" target="_blank">
+      <v-icon class="brand" id="linkedin"  name="brands/linkedin"       />
+    </a>
   </div>
 </template>
 
