b9413f940941cba7d33e9fad16f7cd6eeecc5fd3
Arjay Jones <robert.jones@arjaycodes.com>
2019-05-09 15:43:42 -0700

    added an EmptyComponentTemplate for faster component creation in the future without hand writing boilerplate code every time

---
+++ src/components/helpers/EmptyComponentTemplate.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="COMPONENT_NAME">
+    <!-- ... -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "COMPONENT_NAME",
+  components: {},
+  props: {},
+  methods: {},
+  mounted() {},
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  /* Page-global Variables  */
+  /* Sub-component Variables*/
+  /*------------------------*/
+  /* Styles by Element */
+  /* Styles by Class   */
+  /* Styles by ID      */
+</style>
+
+
