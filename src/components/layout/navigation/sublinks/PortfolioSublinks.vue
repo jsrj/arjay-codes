8e014c2e98c57a479210f4aba5c05602ebf33406
Arjay Jones <robert.jones@arjaycodes.com>
2019-05-10 15:35:36 -0700

    added a component stub for portfolio related sublinks within the About view

---
+++ src/components/layout/navigation/sublinks/PortfolioSublinks.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="portfolio-sublinks">
+    <!-- ... -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PortfolioSublinks",
+  components: {},
+  props: {},
+  methods: {},
+  mounted() {},
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  /* Page-global Variables  */
+  /* Sub-component Variables*/
+  /*------------------------*/
+  /* Styles by Element */
+  /* Styles by Class   */
+  /* Styles by ID      */
+</style>
+
+
