4fa79d7e4a137cb99d24dc4cc6bd1d91a53b87ea
Arjay Jones <robert.jones@arjaycodes.com>
2019-05-10 15:59:15 -0700

    added stub for a Content generic component

---
+++ src/components/content/Content.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="content">
+    <!-- ... -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Content",
+  components: {},
+  props: {},
+  methods: {},
+  mounted() {},
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  /* Page-global Variables  */
+  /* Sub-component Variables*/
+  /*------------------------*/
+  /* Styles by Element */
+  /* Styles by Class   */
+  /* Styles by ID      */
+</style>
+
+
