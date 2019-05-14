99ea5b31418794ee5cec3ee47304cbe905612e64
Arjay Jones <robert.jones@arjaycodes.com>
2019-05-14 00:16:15 -0700

    created a simple navigation component for scrolling a page back to the top

---
+++ src/components/layout/navigation/TopReturn.vue
@@ -0,0 +1,48 @@
+<template>
+<v-container>
+    <v-btn fab small flat
+      id="back-to-top"
+      :fixed="true"
+      :right="true"
+      :bottom="true"
+      v-on:click.native="scrollToTop"
+    >
+      <v-icon name="chevron-up" />
+    </v-btn>
+</v-container>
+</template>
+
+<script>
+export default {
+  name: "TopReturn",
+  methods: {
+    scrollToTop: () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  @primary: rgba(51, 51, 51, 1);
+  @secondary: rgba(255, 166, 0, 1);
+
+  #back-to-top::before {
+    color: transparent !important;
+    background-color: transparent !important;
+  }
+  #back-to-top {
+    color: @secondary;
+    background-color: @primary;
+  }
+  #back-to-top:hover {
+    color: @primary;
+    background-color: @secondary;
+  }
+</style>
+
+
