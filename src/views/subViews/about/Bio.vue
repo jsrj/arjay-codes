5b43397fe3ef8fa276db61888a19bfb29f0eaacf
Arjay Jones <robert.jones@arjaycodes.com>
2019-05-13 22:25:08 -0700

    added hardcoded content placeholder components to About-Bio sub view

--- src/views/subViews/about/Bio.vue
+++ src/views/subViews/about/Bio.vue
@@ -1,18 +1,23 @@
-<template>
+<template :extends="About">
   <div id="about-bio">
-    <!-- ... -->
+    <Content />
+    <Content />
   </div>
 </template>
 
 <script>
 import About from "@/views/About.vue";
+import Content from "@/components/content/Content.vue";
 
 export default {
   extends: About,
   name: "About-Bio",
   components: {},
   props: {},
-  methods: {},
+  methods: {
+    // TODO: Add method to fetch content data blocks from CMS service
+  },
+  created() {},
   mounted() {},
   data() {
     return {}
