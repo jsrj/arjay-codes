39efb8ae949916b88e42a160a67394e7e537a8ca
Arjay Jones <robert.jones@arjaycodes.com>
2019-05-15 08:10:25 -0700

    blocked out the structure of the NavGem menu; added a v-sheet to model a hamburger menu on top of within the NavGem

--- src/components/layout/navigation/experimental/NavGem.vue
+++ src/components/layout/navigation/experimental/NavGem.vue
@@ -1,6 +1,51 @@
 <template>
   <div id="nav-gem">
+    <!-- Positioning and behavior element -->
+    <v-speed-dial large fixed top left
+      id="container"
+      direction="right"
+      transition="slide-x"
+      :open-on-hover="true"
+    >
+      <!-- NavGem primary display element -->
+      <template v-slot:activator>
+        <v-btn large fab id="main-badge" color="rgba(255, 166, 0, 1)">
+          <v-sheet id="menu-icon" flat>
+            <div class="h-bar"></div>
+            <div class="h-bar"></div>
+            <div class="h-bar"></div>
+          </v-sheet>
+        </v-btn>
+      </template>
 
+
+          <!-- NavGem menu items #START -->
+          <v-btn
+            fab
+            dark
+            small
+            color="green"
+          >
+            <v-icon>edit</v-icon>
+          </v-btn>
+          <v-btn
+            fab
+            dark
+            small
+            color="indigo"
+          >
+            <v-icon>add</v-icon>
+          </v-btn>
+          <v-btn
+            fab
+            dark
+            small
+            color="red"
+          >
+        <v-icon>delete</v-icon>
+      </v-btn>
+      <!-- NavGem menu items #END -->
+    </v-speed-dial>
   </div>
 </template>
 
@@ -19,9 +64,41 @@ export default {
 </script>
 
 <style lang="less" scoped>
-// #nav-gem {
-  
-// }
+  @primary: rgba(33, 33, 33, 1) !important;
+  @secondary: rgba(255, 166, 0, 1) !important;
+  @tertiary: rgba(145, 198, 250,1 ) !important;
+  @scaleX: 100%;
+  @scaleY: 10vh;
+  #container {
+    padding: 8px;
+    background-color: @primary;
+    border-radius: 100%;
+  }
+
+  #main-badge {
+    overflow: hidden;
+    background-color: @tertiary;
+
+    // border-radius: 100%;
+  }
+
+  #menu-icon::before{
+    color: transparent;
+    background-color: transparent;
+  }
+  #menu-icon {
+    display: flex;
+    flex-direction: column;
+
+    .h-bar {
+      margin: 0px;
+      padding: 0px;
+      
+      width: 10px;
+      height: 1px;
+      border: 2px solid black;
+    }
+  }
 </style>
 
 
