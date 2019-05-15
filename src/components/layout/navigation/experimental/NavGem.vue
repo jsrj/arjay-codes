1f0d969b5bf789f4b5a5d6210acd26e42ace23ff
Arjay Jones <robert.jones@arjaycodes.com>
2019-05-15 05:04:49 -0700

    added experimental subdirectory to components for drafting navigation refactor into floating gem/badge

---
+++ src/components/layout/navigation/experimental/NavGem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="nav-gem">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavGem",
+  components: {},
+  props: {},
+  methods: {},
+  data () {
+    return {}
+  },
+  created () {},
+  mounted() {}
+}
+</script>
+
+<style lang="less" scoped>
+// #nav-gem {
+  
+// }
+</style>
+
+
