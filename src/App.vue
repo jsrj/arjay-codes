3a2420c7fa5f301bea2cc71f8106d330a1f4948c
Arjay Jones <robert.jones@arjaycodes.com>
2019-06-05 14:31:07 -0700

    added Logo component directly to App layout

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <v-app id="root">
-    <!-- <Navbar /> -->
-    <NavGem />
+    <Logo id="ac-logo" />
+    <NavGem id="" />
     <router-view :title="title" />
   </v-app>
 </template>
 
 <script>
 // import Navbar from './components/layout/navigation/Navbar.vue';
+import Logo   from './components/content/images/Logo.vue';
 import NavGem from "@/components/layout/navigation/experimental/NavGem.vue";
 import Footer from './components/layout/Footer.vue';
 
 export default {
   components: {
-    // Navbar,
+    Logo,
     NavGem,
     Footer
   },
@@ -34,14 +35,24 @@ export default {
 </script>
 
 
-<style>
-#root {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  height: 100% !important;
-  /* background-color: rgba(98, 27, 193, 1); */
-}
+<style lang="less">
+  @neutralLightColor: rgba(245, 245, 245, 1) !important;
+  @neutralDarkColor:  rgba(78 , 78 , 78 , 1) !important;
+  @primaryColor:      rgba(255, 166, 0  , 1) !important;
+  @secondaryColor:    rgba(145, 198, 250, 1) !important;
+  #root {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+
+    // overflow: hidden;
+
+    height: 100vh !important;
+
+    background-color: @neutralLightColor;
+    border: 7px solid @neutralLightColor;
+    color: @neutralDarkColor;
+  }
+
 </style>
