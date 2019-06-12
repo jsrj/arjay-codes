a1fe141702d7fa7eb69e7f81e7c37c914d9c3ace
Arjay Jones <robert.jones@arjaycodes.com>
2019-06-11 18:50:59 -0700

    cleaned up no-op data and added a simple structure to Home page content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,64 +3,51 @@
     <Meta 
       :title="title" 
       :subTitle="subTitle" 
-      :description="description" 
     />
+    <h1>Independent Web Developer</h1>
+    <Content />
+    <v-btn class="cta">I Need A Website</v-btn>
+    <v-btn class="cta">I Need A Contributor</v-btn>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Meta from '../components/helpers/Meta.vue';
+import Content from '@/components/content/Content.vue';
 
 export default {
   name: "Home",
   components: {
-    Meta
+    Meta,
+    Content
   },
   props: {
     title: String
   },
   data() {
     return {
-      subTitle: 'Home',
-      description: 'Home of Arjay | Codes',
-      contents: [
-        {
-          head: 'Selling Points',
-          block: 'List selling points of Arjay Codes here'
-        },
-        {
-          head: 'Marketplace Highlights',
-          block: 'Showcase certain aspects of the Arjay Codes marketplace'
-        },
-        {
-          head: 'Flat Rate Services',
-          block: 'Detail the different types of services that are provided at a flat rate.'
-        }
-      ],
-      lastActionPanels: [
-        {
-          title: 'Hire Arjay|Codes',
-          description: 'Description summarizing why you should hire Arjay|Codes',
-          features: ['feature one', 'feature two', 'feature red', 'feature blue'],
-          actionCall: 'Hire Me'
-        },
-        {
-          title: 'Join Arjay|Codes',
-          description: 'Description summarizing why developers/designers/artists should partner with Arjay|Codes.',
-          features: ['feature one', 'feature two', 'feature red', 'feature blue'],
-          actionCall: 'Join Me'
-        }
-      ]
+      subTitle: 'Home'
     }
   }
 };
 </script>
 
 <style lang="less">
+  h1 {
+    font-size: 80px !important;
+  }
   #home {
     width: 100vw;
     height: 100vh;
-    // background-color: #366ed8;
+  }
+  .cta {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 25px;
+    background-color: rgba(232, 60 , 56 , 1);
+    color: rgba(245, 245, 245, 1);
   }
 </style>
